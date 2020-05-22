9907acdeb8a411f9c0c3e765ef303b13df1fcacb
David <davidgugea@yahoo.com>
2020-05-22 19:06:19 +0200

    Example of docstrings & running pydoc in console for time&math modules ( for windows : python -m pydoc *.py )

---
+++ doc2_test.py
@@ -0,0 +1,11 @@
+class TestClass(object):
+    """This is a test class
+
+    It doesn't do anything, it doesn't not have any methods or properties. It is useless"""
+    pass
+
+def test():
+    """This is a test function
+
+    It doesn't do anything."""
+    pass
